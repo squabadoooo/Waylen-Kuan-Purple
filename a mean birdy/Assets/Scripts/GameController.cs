757e0953673ecec79914b5c0d1bd03973a54bae7
Cleo <107956818+squabadoooo@users.noreply.github.com>
2022-07-06 18:03:57 -0400

    Latest Changes

--- a mean birdy/Assets/Scripts/GameController.cs
+++ a mean birdy/Assets/Scripts/GameController.cs
@@ -5,10 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Game Over UI Object for displaying Game Over Screen")]
+    public GameObject gameOverCanvas;
+    [Header("Score UI Object for displaying Score")]
+    public GameObject scoreCanvas;
+    [Header("Spawner Object for spawning objects in game")]
+    public GameObject spawner;
     // Start is called before the first frame update
     void Start()
     {
-
+        Time.timeScale = 1;
+        scoreCanvas.SetActive(true);
+        gameOverCanvas.SetActive(false);
+        spawner.SetActive(true);
     }
 
     // Update is called once per frame
