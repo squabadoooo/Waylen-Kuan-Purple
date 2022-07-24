1fb20afeaf26e5b0242d1366cc7c1f4bd0873887
anna banana <107956818+squabadoooo@users.noreply.github.com>
2022-07-24 13:03:40 -0400

    sketchHead

---
+++ WK GucciHead/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Rigidbody")]
+    public Rigidbody2D rb;
+    [Header("Default Down Speed")]
+    public float downSpeed = 20f;
+    [Header("Default Movement Speed")]
+    public float movementSpeed = 10f;
+    [Header("Default Directional movement Speed")]
+    public float movement = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
+            }
+}
