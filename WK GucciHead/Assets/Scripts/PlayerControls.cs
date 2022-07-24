d8334641f7b03035e36d74ae97e8dde2b9ed24f7
Waylen <waylenkuan@gmail.com>
2022-07-24 14:47:06 -0400

    po is baby, added gitignore
    me = 17 step

--- WK GucciHead/Assets/Scripts/PlayerControls.cs
+++ WK GucciHead/Assets/Scripts/PlayerControls.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerControls : MonoBehaviour
 {
@@ -23,5 +23,25 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal") * movementSpeed;
-            }
+        if (movement < 0)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, downSpeed, 0);
+    }
 }
+    
