05b7635ab6d2f93b9bb5c88f0856419591da5018
Aroosh <>
2022-07-26 19:00:45 -0400

    po

--- WK GucciHead/Assets/Scripts/GameController.cs
+++ WK GucciHead/Assets/Scripts/GameController.cs
@@ -5,15 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Platform Object")]
+    public GameObject platform;
+    float pos = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        for(int i = 0; i <1000; i++)
+        {
+            SpawnPlatforms();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void SpawnPlatforms()
+    {
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
     }
 }
