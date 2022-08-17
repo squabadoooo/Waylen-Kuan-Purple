83c385558b6fc66171e55095cbc4e19bd4d6cd6b
squabadoooo <waylenkuan@gmail.com>
2022-08-17 17:58:10 -0400

    question 33

--- wk-Super squars/Assets/Scripts/GameController.cs
+++ wk-Super squars/Assets/Scripts/GameController.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Shape Object")]
+    public GameObject[] shapePrefabs;
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 3f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 3f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
--- wk-Super squars/Assets/Scripts/PlayerControls.cs
+++ wk-Super squars/Assets/Scripts/PlayerControls.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("Default Movement Speed")]
+    public float moveSpeed = 600f;
+    float movement = 0f;
     // Start is called before the first frame update
     void Start()
     {
-
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
 
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Time.timeScale = 0;
     }
 }
