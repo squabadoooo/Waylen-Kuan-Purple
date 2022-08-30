a4c3b01b1a75e55926c4480d5848cdb9bb84f017
Aroosh <107956818+squabadoooo@users.noreply.github.com>
2022-08-30 17:51:50 -0400

    poo
    num46 on

--- PixelWalk/Assets/Scripts/PlayerControls.cs
+++ PixelWalk/Assets/Scripts/PlayerControls.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     [Header("Default Jumping Power")]
-    public float jumpPower = 69f;
+    public float jumpPower = 6f;
     [Header("Boolean isGrounded")]
     public bool isGrounded = false;
     float posX = 0.0f;
@@ -25,9 +25,33 @@ public class PlayerControls : MonoBehaviour
 
     void FixedUpdate()
     {
-    if (Input.GetKey(KeyCode.Space) && isGrounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector3.up * (jumpPower * rb.mass * rb.gravityScale * 20.0f));
         }       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
--- PixelWalk/Assets/Scripts/Spawner.cs
+++ PixelWalk/Assets/Scripts/Spawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("ChallengeObj Game Object")]
+    public GameObject challengeObject;
+    [Header("Default spawn delay time")]
+    public float spawnDelay = 0.6f;
+    [Header("Default spawn time")]
+    public float spawnTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("InstantiateObjects", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.position = new Vector3(15, -4f, 0);
+    }
+    void InstantiateObjects()
+    {
+        Instantiate(challengeObject, transform.position, transform.rotation);
     }
 }
