d28cab19908a8971a4056e5c2fd2353bd2d1617d
Aroosh <aroosh.manha@gmail.com>
2022-09-13 17:48:35 -0400

    on dropping bombs pt 2

--- stack and break/Assets/GameController.cs
+++ stack and break/Assets/GameController.cs
@@ -32,17 +32,19 @@ public class GameController : MonoBehaviour
         var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
         var pos1 = lastCube.transform.position + Vector3.up * 10f;
         var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
-        if(Level % 2 == 0)
+        var pos3 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * -120;
+        if (Level % 2 == 0)
         {
-            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+            currentCube.transform.position = Vector3.Lerp(pos2,pos3, time);
         }
         else
         {
-            currentCube.transform.position = Vector3.Lerp(pos1, pos2, time);
+            currentCube.transform.position = Vector3.Lerp(pos3, pos2, time);
         }
         if (Input.GetMouseButtonDown(0))
         {
             NewBlock();
+            text.text = "Score:" + Level;
         }
     }
     void NewBlock()
@@ -55,6 +57,10 @@ public class GameController : MonoBehaviour
             currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
                 lastCube.transform.localScale.y,
                 lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+
+            currentCube.transform.position =
+             Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+
             if (currentCube.transform.localScale.x <= 0f ||
                 currentCube.transform.localScale.z <= 0f)
             {
@@ -67,7 +73,7 @@ public class GameController : MonoBehaviour
         }
         lastCube = currentCube;
         currentCube = Instantiate(lastCube);
-        currentCube.name = Level + "";
+        currentCube.name = Level+ "";
         currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
         Level++;
         Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
