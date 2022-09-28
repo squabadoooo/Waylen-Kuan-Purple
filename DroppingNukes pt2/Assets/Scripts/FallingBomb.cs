19939755e9baec45ed70f03adb476295426b7df6
Aroosh <aroosh.manha@gmail.com>
2022-09-28 17:49:23 -0400

    done dropping bombs pt2

---
+++ DroppingNukes pt2/Assets/Scripts/FallingBomb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBomb : MonoBehaviour
+{
+    void Start()
+    {
+        float randomX = Random.Range(10f, 100f);
+        float randomY = Random.Range(10f, 100f);
+        float randomZ = Random.Range(10f, 100f);
+
+        Rigidbody bomb = GetComponent<Rigidbody>();
+        bomb.AddTorque(randomX, randomY, randomZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ DroppingNukes pt2/Assets/Scripts/RocketAnimate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketAnimate : MonoBehaviour
+{
+    [Header("Animator")]
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("HAxis", horizontal);
+    }
+}
