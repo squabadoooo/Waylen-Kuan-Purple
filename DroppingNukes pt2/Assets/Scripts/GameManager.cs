e074142ba18ddf8598362a2e919ee6c2d7da20f8
alison <107956818+squabadoooo@users.noreply.github.com>
2022-10-04 17:50:14 -0400

    po po

---
+++ DroppingNukes pt2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Spawner spawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawner.active = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            spawner.active = true;
+        }
+    }
+    private void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+    }
+}
