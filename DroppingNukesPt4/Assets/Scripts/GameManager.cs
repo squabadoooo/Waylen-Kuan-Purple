4d3f0f74bd9333d4936f20996a99011eb5555008
Cassidy <107956818+squabadoooo@users.noreply.github.com>
2022-10-27 17:50:01 -0400

    Created new project for part 4

---
+++ DroppingNukesPt4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+    public GameObject splash;
+    public GameObject scoreSystem;
+    public Text scoreText;
+    public int pointsWorth = 1;
+    private int score;
+
+    private void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
+        scoreText.enabled = false;
+    }
+
+
+// Start is called before the first frame update
+void Start()
+    {
+        spawner.active = false;
+        title.SetActive(true);
+        splash.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!gameStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                ResetGame();
+            }
+        }else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (!gameStarted)
+            {
+                Destroy(bombObject);
+            } else if (bombObject.transform.position.y < (-screenBounds.y) && gameStarted)
+            {
+                scoreSystem.GetComponent<Score>().AddScore(pointsWorth);
+                Destroy(bombObject);
+            }
+        }
+    }
+
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        splash.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+
+        scoreText.enabled = true;
+        scoreSystem.GetComponent<Score>().score = 0;
+        scoreSystem.GetComponent<Score>().Start();
+    }
+
+    void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+        title.SetActive(false);
+        splash.SetActive(true);
+    }
+}
