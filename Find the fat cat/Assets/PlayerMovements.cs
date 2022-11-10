76c693eb6eb279cb6a68d0635cea90798beded72
Aroosh <107956818+squabadoooo@users.noreply.github.com>
2022-11-10 18:00:04 -0500

    finished two first projects try the code again for the PF

---
+++ Find the fat cat/Assets/PlayerMovements.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //try here again
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+float horizontal = Input.GetAxis("Horizontal");
+float vertical = 0.80f;
+Vector3 destination = new Vector3(horizontal, 0, vertical);
+transform.Translate(destination * speed * Time.deltaTime);
+*/
