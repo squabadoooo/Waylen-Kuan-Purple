a915866d8c9e3629b248941c7c8b0b7010f0bd19
Kelton <107956818+squabadoooo@users.noreply.github.com>
2022-11-17 18:01:10 -0500

    need to explain code next time po

---
+++ forest run/Assets/Scripts/Animate.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    Animator Animator;
+    Jump Jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        Jump = GetComponent<Jump>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Jump.isGrounded){
+            Animator.SetBool("isJumping", true);
+            Animator.SetBool("isIdle", false);
+            Animator.SetBool("isWalking", false);
+            Animator.SetBool("isWalkingBackwards", false);
+        }
+
+        if (Jump.isGrounded)
+        {
+            Animator.SetBool("isIdle", true);
+            Animator.SetBool("isJumping", false);
+            Animator.SetBool("isWalking", false);
+            Animator.SetBool("isWalkingBackwards", false);
+            if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                Animator.SetBool("isWalking", true);
+                Animator.SetBool("isIdle", false);
+                Animator.SetBool("isWalkingBackwards", false);
+            }
+            if (Input.GetAxisRaw("Vertical") == -1)
+            {
+                Animator.SetBool("isWalkingBackwards", true);
+                Animator.SetBool("isWalking", false);
+                Animator.SetBool("isIdle", false);
+            }
+        }
+    }
+}
---
+++ forest run/Assets/Scripts/Jump.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody u;
+
+    float jumpForce = 5.7f;
+
+    public bool isGrounded;
+
+    float fallmultiplier = 1.5f;
+
+    void Start()
+    {
+        u = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, .15f);
+        Debug.DrawRay(transform.position, Vector3.down * .15f, Color.red);
+        
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            u.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if(u.velocity.y < 0){
+            u.velocity += Physics.gravity * fallmultiplier * Time.deltaTime;
+        }
+    }
+}
+
