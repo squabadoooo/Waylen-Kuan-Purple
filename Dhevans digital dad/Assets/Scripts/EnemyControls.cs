83a44abce4d0ef1743ef73236aa69569d3a67bf8
rebecca <107956818+squabadoooo@users.noreply.github.com>
2022-12-27 14:53:37 -0500

    ON STEP 17B

--- Dhevans digital dad/Assets/Scripts/EnemyControls.cs
+++ Dhevans digital dad/Assets/Scripts/EnemyControls.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class EnemyControls : MonoBehaviour
 {
     public float speed = 5;
-    public float attakingDistance = 1;
+    public float attackingDistance = 1;
     public Vector3 direction;
 
     private Animator animatorEnemy;
     private Rigidbody rigidbodyEnemy;
     private Transform target;
+
     public bool isFollowingTarget;
     public bool isAttackingTarget;
-    public float currentAttakingTime;
-    public float maxAttakingTime = 2f;
+    private float chasingPlayer = 0.01f;
+    public float currentAttackingTime;
+    public float maxAttackingTime = 2f;
     // Start is called before the first frame update
     void Start()
     {
         isFollowingTarget = true;
-        currentAttakingTime = maxAttakingTime;
+        currentAttackingTime = maxAttackingTime;
+        animatorEnemy = GetComponent<Animator>();
+        rigidbodyEnemy = GetComponent<Rigidbody>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
@@ -27,4 +32,41 @@ public class EnemyControls : MonoBehaviour
     {
         
     }
-}
+
+    void Followtarget()
+    {
+        if (!isFollowingTarget)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, target.position) >= attackingDistance)
+        {
+            direction = target.position - transform.position;
+            direction.y = 0;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 20);
+
+            if (rigidbodyEnemy.velocity.sqrMagnitude != 0)
+            {
+                rigidbodyEnemy.velocity = transform.forward * speed;
+                animatorEnemy.SetBool("Walk", true);
+            }
+        }
+
+        else if(Vector3.Distance(transform.position,target.position) <= attackingDistance)
+        {
+            rigidbodyEnemy.velocity = Vector3.zero;
+            animatorEnemy.SetBool("Walk", false);
+            isFollowingTarget = false;
+            isAttackingTarget = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Followtarget();
+    }
+
+  
+        }
