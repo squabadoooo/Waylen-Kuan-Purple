cbc098700c39402449166125ea644becbe0a9338
rebecca <107956818+squabadoooo@users.noreply.github.com>
2023-01-11 17:57:14 -0500

    yes

---
+++ Yes/Assets/Scripts/BennyIsGone.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BennyIsGone : MonoBehaviour
+{
+
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("coin"))
+        {
+            Destroy(other.gameObject);
+            Destroy(door.gameObject);
+            Destroy()
+        }
+
+
+    }
+}
