d0bea8aa6633e25b87c42f2d4c8ba767bae481b9
Brandon <107956818+squabadoooo@users.noreply.github.com>
2023-01-26 17:48:37 -0500

    on py of colors
    yese

--- WorldOfRobotBENNYSw.k/Assets/Scripts/LevelReset.cs
+++ WorldOfRobotBENNYSw.k/Assets/Scripts/LevelReset.cs
@@ -5,11 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelReset : MonoBehaviour
 {
+    public ParticleSystem explosion;
+    
     public GameObject player;
+
+    private void Start()
+    {
+        explosion.Stop();
+    }
     public void GameOver()
     {
         player.SetActive(false);
+        Invoke("reload", 2);
+        explosion.Play();
+    }
+
+    void Reload()
+    {
         SceneManager.LoadScene(0);
+    }
+
+    private void FixedUpdate()
+    {
+        explosion.transform.position = player.transform.position;
     }
 }
 
