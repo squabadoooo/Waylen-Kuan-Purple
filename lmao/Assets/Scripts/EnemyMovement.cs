1f4e648f1e47d75fa8bb62385ad8137922ce3a51
#MidlifeCrisis <107956818+squabadoooo@users.noreply.github.com>
2023-02-16 17:58:25 -0500

    yes

---
+++ lmao/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float yForce;
+    public float xForce;
+    public float xDirection;
+    private Rigidbody2D enemyRigidBody;
+
+    void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position.x <= -8)
+        {
+            xDirection = 1;
+            enemyRigidBody.AddForce(Vector2.right * xForce);
+        }
+        if (transform.position.x >= 8)
+        {
+            xDirection = -1;
+            enemyRigidBody.AddForce(Vector2.left * xForce);
+        }
+    }
+}
