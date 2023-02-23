ccd36141e23da6d13538ef09c01f20cbb094d567
Artbopper <107956818+squabadoooo@users.noreply.github.com>
2023-02-23 17:58:42 -0500

    forgor what step im on

---
+++ qpDePo/Assets/Scripts/Exit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//waylen is stupid
+public class Exit : MonoBehaviour
+{
+    public GameObject gem;
+    public GameObject background;
+    public string teleportDestination;
+}
