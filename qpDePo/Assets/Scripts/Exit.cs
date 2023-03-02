c88991238a61fae940c65790ac63ca027cc8b1f2
Artbopper <126628086+Bamber22@users.noreply.github.com>
2023-03-02 17:59:24 -0500

    still forgor what step im on

--- qpDePo/Assets/Scripts/Exit.cs
+++ qpDePo/Assets/Scripts/Exit.cs
@@ -7,4 +7,11 @@ public class Exit : MonoBehaviour
     public GameObject gem;
     public GameObject background;
     public string teleportDestination;
+
+    private void OnTriggerEnter(Collider other)
+    {if (gem.activeInHierarchy == false)
+        {
+            background.GetComponent<GameManager>().TeleportOpen(teleportDestination);
+        }
+    }
 }
