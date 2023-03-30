8163cc1810e8c2621fb1f6e8605e5a1e11cfe36a
ur mum <107956818+squabadoooo@users.noreply.github.com>
2023-03-30 17:59:02 -0400

    popop

--- qpDePo/Assets/Scripts/Exit.cs
+++ qpDePo/Assets/Scripts/Exit.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 //waylen is stupid
 public class Exit : MonoBehaviour
 {
+
     public GameObject gem;
     public GameObject background;
     public string teleportDestination;
-
+    private void Start()
+    {
+        
+    }
     private void OnTriggerEnter(Collider other)
     {if (gem.activeInHierarchy == false)
         {
-            background.GetComponent<GameManager>().TeleportOpen(teleportDestination);
+            //background.GetComponent<GameManager>().TeleportOpen(teleportDestination);
+            background.GetComponent<GameManager>().TeleportOpen(nextScene: teleportDestination);
         }
     }
 }
